2448de562f0330114a6e30c2bc6bc3aff985c197
andrebeu <aobeukers@gmail.com>
2019-08-23 10:04:25 -0400

    sweep: randomizing every ep

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -r -vam abeukers@tigergpu.princeton.edu:/tigress/abeukers/wd/pm/model_data/PITask_many_ntokens/* /Users/abeukers/wd/pm/model_data/PITask_many_ntokens/
+rsync -r -vam abeukers@tigergpu.princeton.edu:/tigress/abeukers/wd/pm/model_data/PITask_randstim/* /Users/abeukers/wd/pm/model_data/PITask_randstim/
