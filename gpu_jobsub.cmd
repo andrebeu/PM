f365bc521f12b63621cf0b0821a916bb92b95b20
andrebeu <aobeukers@gmail.com>
2019-10-03 16:56:50 -0400

    amtask sweep1

--- gpu_jobsub.cmd
+++ gpu_jobsub.cmd
@@ -10,16 +10,13 @@
 #SBATCH --gres=gpu:0		# number of gpus 4
 
 seed=${1}
-ntrials=${2}
-stsize1=${3} 
-stsize2=${4} 
 
 module load anaconda3/4.4.0
 module load cudnn/cuda-9.1/7.1.2
 
 printf "\n\n complex maps task \n\n"
 
-srun python -u "/tigress/abeukers/wd/pm/exp-dual_lstms-sweep2.py" ${seed} ${ntrials} ${stsize1} ${stsize2}
+srun python -u "/tigress/abeukers/wd/pm/exp-amtask-sweep1.py" ${seed} 
 
 
 printf "\n\nGPU profiling \n\n"
